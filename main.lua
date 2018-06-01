74d929cf7ed09e9276d2a0990cf2bdcc07f41451
Mouaffakkub <36240791+Mouaffakkub@users.noreply.github.com>
2018-05-31 19:30:25 -0600

    a

--- main.lua
+++ main.lua
@@ -1,7 +1,45 @@
 -----------------------------------------------------------------------------------------
---
--- main.lua
---
+-- Created by: Mouaffak Koubeisy
+-- Created on: May 31st 2018
 -----------------------------------------------------------------------------------------
 
--- Your code here
+local numberTextField = native.newTextField( display.contentCenterX , display.contentCenterY - 300, 450, 75 )
+numberTextField.id = "number textField"
+
+local numberField = display.newText( " Input Number ", display.contentCenterX , display.contentCenterY - 500, native.systemFont, 85 )
+numberField.id = "number textField"
+numberField:setFillColor( 1, 1, 1 )
+
+local rightField = display.newText( "", display.contentCenterX , display.contentCenterY + 500, native.systemFont, 85 )
+rightField.id = "right textField"
+rightField:setFillColor( 1, 1, 1)
+
+local lowerField = display.newText( "", display.contentCenterX , display.contentCenterY + 500, native.systemFont, 85 )
+lowerField.id = "lower textField"
+lowerField:setFillColor( 1, 1, 1)
+
+local higherField = display.newText( "", display.contentCenterX , display.contentCenterY + 500, native.systemFont, 85 )
+higherField.id = "higher textField"
+higherField:setFillColor( 1, 1, 1)
+
+local calculateButton = display.newImageRect( "./assets/sprites/enterButton.png", 425, 251 )
+calculateButton.x = display.contentCenterX
+calculateButton.y = display.contentCenterY
+calculateButton.id = "calculate button"
+
+local number
+
+local function calculateButtonTouch( event )
+
+    number = tonumber (numberTextField.text)
+    local zero = 0
+
+    if number < zero  then
+        numberField.text = " That number is negative "
+    elseif number > zero  then
+    	numberField.text = " That number is positive "
+    else numberField.text = " That number is zero "
+    return true
+    end
+end
+calculateButton:addEventListener( "touch", calculateButtonTouch )
